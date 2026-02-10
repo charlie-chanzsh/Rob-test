42859cb2185ab30ee04cc22ce2fda4b450234b9c
Charlie Chan <09charliechan@gmail.com>
2026-02-10 17:18:42 +0700

    local script

---
+++ 3am/place1/service.lua
@@ -0,0 +1,9 @@
+local TeleportService = game:GetService("TeleportService")
+
+local placeId = 131558113730241
+
+game.Players.PlayerAdded:Connect(function(player)
+	TeleportService:TeleportAsync(placeId , {player})
+end)
+
+
--- learnlua/main.lua
+++ learnlua/main.lua
@@ -3,3 +3,16 @@
 local base = Game.Workspace
 
 base = ""
+
+
+
+
+
+
+
+
+
+
+
+
+
